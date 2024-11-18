2f26f5492a747527af643fbea2d5092e72941aff
Armando Trapaga <aggonzal@lcg.unam.mx.com>
2024-11-18 15:33:28 -0600

    Integracion del logger script expresion diferencial

--- src/Expresion_diferencial.py
+++ src/Expresion_diferencial.py
@@ -1,7 +1,23 @@
 import pandas as pd
 from pydeseq2.dds import DeseqDataSet
+import logging
 import argparse 
+import sys
 import os
+
+def getlogger(*args,**kwargs):
+
+    logger = logging.getLogger('Analisis diferencial')
+    logger.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler() 
+    console_handler.setLevel(logging.INFO) 
+    # Crear un formateador y añadirlo al manejador 
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s') 
+    console_handler.setFormatter(formatter) 
+    # Añadir el manejador al logger 
+    logger.addHandler(console_handler)
+    return logger
 
 def process_list(argline):
     if os.path.isfile(argline):
@@ -19,18 +35,28 @@ def analisis_diferencial(table:str, samples:dict) -> pd.DataFrame:
     Returns: 
         pd.Datafreme: Retorna un dataframe con los valores de expresion base y diferencial a "states"
     '''
+    
+    logger = getlogger()
+    
+    sys.stderr = open(os.devnull, 'w')
+
+    logger.info('Iniciando analisis diferencial')
     try:    
+        logger.info('Leyendo matriz de conteos')
         # Leer la matriz de conteo
         count_matrix = pd.read_csv(table,index_col=0).T
         if count_matrix.empty:
-            raise ValueError("EL dataframe cargado esta vacio")
+            logger.critical("EL dataframe cargado esta vacio")
+            raise
         count_matrix = count_matrix.loc[[sample for group in samples.values() for sample in (group if isinstance(group,list) else [group])]]
-
+        logger.info('Parseando matriz de conteos')
         # Reenombrar las filas en el orden deseado
         count_matrix = count_matrix.rename(index={sample : (group + sample[-1]) for group,cases in samples.items() for sample in (cases if isinstance(cases,list) else [cases])})
         count_matrix = count_matrix.round().astype(int)
     except Exception as e:
-        raise(f'Error creando la matriz de conteos: {e}')
+        logger.critical(f'Error creando la matriz de conteos: {e}')
+        raise
+    logger.info('Empezando analasis deferencial')
     try:
         # Definir la metadata con "low-mg" como la condición de referencia
         metadata = pd.DataFrame({
@@ -47,10 +73,9 @@ def analisis_diferencial(table:str, samples:dict) -> pd.DataFrame:
         # Ejecutar el análisis DESeq2
         dds.deseq2()
 
-        # Tabla Log natural
-        dds.varm['LFC']
     except Exception as e:
         raise(f'Error al realizar el analisis diferenical: {e}')
+    logger.info('Terminando analalisis diferencial')
     return dds.varm['LFC']
 
 if __name__ == "__main__":
