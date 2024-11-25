1884a30eb4b1f62a0e7eeb67c07ec1381beaa6b6
Armando Trapaga <aggonzal@lcg.unam.mx.com>
2024-11-24 20:35:48 -0600

    Resolucion de conflicto

---
+++ src/logger.py
@@ -0,0 +1,27 @@
+# ===========================================================================
+# =                            imports
+# ===========================================================================
+import logging
+# ===========================================================================
+# =                            functions
+# ===========================================================================
+
+def getlogger(*args,**kwargs):
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler() 
+    console_handler.setLevel(logging.INFO) 
+    # Crear un formateador y añadirlo al manejador 
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s') 
+    console_handler.setFormatter(formatter) 
+    # Añadir el manejador al logger 
+    logger.addHandler(console_handler)
+    return logger
+
+# ===========================================================================
+# =                            main
+# ===========================================================================
+if __name__ == "__main__":
+    pass
